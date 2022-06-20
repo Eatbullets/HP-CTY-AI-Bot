45d10b28002768867ea393fc346cf0da36b0400e
sahanashettigar <r.sahana.shettigar@gmail.com>
2022-06-20 22:43:44 +0530

    Tried login fixes

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,24 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class AutheticatedAction(Action):
+
+    def name(self) -> Text:
+        return "action_autheticated"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        email=tracker.get_slot("email")
+        otp=tracker.get_slot("otp")
+        if email is not None and otp is not None:
+            dispatcher.utter_message(template="utter_autheticated_successfully")
+        else:
+            dispatcher.utter_message(template="utter_authetication_failed")
+
+        return []
